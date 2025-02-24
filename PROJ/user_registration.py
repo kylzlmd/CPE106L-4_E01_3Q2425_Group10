a77341883e7fe1ae7c2499c34d8c5fe0e5b8a0c5
Kayla Zalameda <160977337+kylzlmd@users.noreply.github.com>
2025-02-24 13:32:06 +0800

    Made some changes in user_registration.py

--- PROJ/user_registration.py
+++ PROJ/user_registration.py
@@ -4,20 +4,35 @@ import os
 import getpass  # Importing getpass to hide password input
 
 class User:
-    def __init__(self, filename="users.json"):
-        self.filename = filename
-        self.users = self.load_users()  # Load existing users if the file exists
+    volunteer_id_counter = 1
+    
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = self.hash_password(password)
+        self.role = role
+        self.volunteer_id = None
 
     def hash_password(self, password):
-        """Hashes the password using SHA-256 for security."""
         return hashlib.sha256(password.encode()).hexdigest()
 
-    def load_users(self):
-        """Loads user data from the JSON file if it exists."""
-        if os.path.exists(self.filename):
-            with open(self.filename, "r") as file:
-                return json.load(file)
-        return {}
+    def register(self):
+        try:
+            with open("users.json", "r") as file:
+                users = json.load(file)
+        except FileNotFoundError:
+            users = []
+        
+        if self.role == "volunteer":
+            self.volunteer_id = User.volunteer_id_counter
+            User.volunteer_id_counter += 1
+            user_data = {"username": self.username, "password": self.password, "role": self.role, "volunteer_id": self.volunteer_id, "approved": False}
+        else:
+            user_data = {"username": self.username, "password": self.password, "role": self.role}
+        
+        users.append(user_data)
+        with open("users.json", "w") as file:
+            json.dump(users, file, indent=4)
+        return True
 
     def save_users(self):
         """Saves the user data to a JSON file."""
